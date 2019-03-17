60801552a86dd09bbea1037812b8b4d5737f087d
david-pm <damcdonald@weedmaps.com>
2019-03-16 23:57:14 -0700

    first commit - create mapping and war_and_peace index

---
+++ lib/config.rb
@@ -0,0 +1,15 @@
+INDEX_NAME = 'war_and_peace'
+CLIENT = Elasticsearch::Client.new(host: 'localhost', port: '9200')
+MAPPING = {
+  body: {
+    mappings: {
+      document: {
+        properties: {
+          book: { type: :text },
+          chapter: { type: :text },
+          contents: { type: :text, analyzer: :english }
+        }
+      }
+    }
+  }
+}.freeze
